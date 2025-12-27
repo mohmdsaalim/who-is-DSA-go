5859c72c34169a199ae0069095795dd28bcb6809
mohmdsaalim <muhammedsaalim005@gmail.com>
2025-12-27 14:02:35 +0530

    LinkedList/Add/Del/Display

--- topics/LinkedList/main.go
+++ topics/LinkedList/main.go
@@ -2,39 +2,86 @@ package main
 
 import "fmt"
 
-type node struct{
+type Node struct{
 	Data int
-	Next *node
-}
-type LinkedList struct{
-	head *node
-	length int
-}
-func (l *LinkedList) prepend(n *node){
-	second := l.head
-	l.head = n
-	l.head.Next = second
-	l.length++
+	Next *Node
 }
 
-func (l LinkedList) printData(){
-	toprint := l.head
-	for l.length != 0{
-		fmt.Printf("%d ", toprint.Data)
-		toprint = toprint.Next
-		l.length--
+type LinkedList struct{
+	Head *Node
+}
+
+func (l *LinkedList) IsEmpty() bool{
+	return l.Head == nil
+}
+// NewList
+func NewList() *LinkedList{
+	return &LinkedList{}
+}
+
+// Append 
+func (l *LinkedList) Append(data int){
+	newNode := &Node{Data: data}
+
+	if l.IsEmpty(){
+		l.Head = newNode
+		return
 	}
-	fmt.Printf("\n")
+
+	current := l.Head
+	for current.Next != nil{
+		current = current.Next
+	}
+	current.Next = newNode
+}
+//____________________________________________________________________________
+//____________________________________________________________________________
+//delete
+func (l *LinkedList) Delete(val int){
+	if l.IsEmpty(){
+		fmt.Println("The List Is Empty")
+		return
+	}
+	if l.Head.Data == val{
+		l.Head = l.Head.Next
+		return
+	}
+	prev := l.Head
+	current := l.Head.Next
+
+	for current != nil{
+		if current.Data == val{
+			prev.Next = current.Next
+			return
+		}
+		prev = current
+		current = current.Next
+	}
+ 	fmt.Printf("value %d not found in the list\n", val)
+}
+// Display 
+
+
+
+func (l *LinkedList) Display(){
+	if l.IsEmpty(){
+		fmt.Println("There is no value in List")
+	}
+	current := l.Head
+	for current != nil{
+		fmt.Printf("%d -> ",current.Data)
+		current = current.Next
+	}
+		fmt.Print("nil")
 }
 
 func main() {
-	myList := LinkedList{}
-	node1 := &node{Data: 10}
-	node2 := &node{Data: 12}
-	node3 := &node{Data: 13}
-	myList.prepend(node1)
-	myList.prepend(node2)
-	myList.prepend(node3)
+list := NewList()
 
-myList.printData()
+list.Append(10)
+list.Append(20)
+list.Append(30)
+
+list.Delete(20)
+list.Display()
 }
