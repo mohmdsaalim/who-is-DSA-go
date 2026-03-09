974b99430dc459b499c3c669711f3c4d8b6487ae
mohmdsaalim <muhammedsaalim005@gmail.com>
2026-03-09 20:10:46 +0530

    hashing completed need todo questions

--- HashTables/array_hashing/array_hashing.go
+++ HashTables/array_hashing/array_hashing.go
@@ -11,3 +11,18 @@ func Hashmapping(a []int,b int)  {
 	}
 	fmt.Println("Count", m[b])
 }
+
+
+func TwoSum(arr []int, a int) []int {
+
+	m := make(map[int]int) 
+	for i, v := range arr{
+		n := a - v
+		// fmt.Println(i, v)
+		if idx, ok := m[n]; ok{ 
+			return []int{idx, i}
+		}
+		m[v]=i 
+	}
+	return []int{}
+}
--- HashTables/main.go
+++ HashTables/main.go
@@ -1,10 +1,15 @@
 package main
 
-import Array_hashing "hashing/array_hashing"
+import (
+	"fmt"
+	Array_hashing "hashing/array_hashing"
+)
 
 
 
 func main() {
 	arr := []int{1,2,12,1,2,12,1,2,12,1,2,12,1,21,2,12}
 	Array_hashing.Hashmapping(arr, 1)
+	ar := Array_hashing.TwoSum(arr, 13)
+	fmt.Println(ar)
 }
