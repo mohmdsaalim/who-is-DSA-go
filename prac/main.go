df6ba77f19849f37d8d7df1048b88d81db5bf514
mohmdsaalim <muhammedsaalim005@gmail.com>
2026-03-16 12:17:11 +0530

    finished the quick_sort algo

--- prac/main.go
+++ prac/main.go
@@ -1,7 +1,37 @@
 package main
 
-func main() {
-	
+import "fmt"
+
+
+func quicksort(arr []int, low, high int)  {
+	if low < high{
+		pi := partition(arr, low, high)
+
+		quicksort(arr, low, pi-1)
+		quicksort(arr, pi+1, high)
+	}
+}
+
+func partition(arr []int, low, high int) int {
+	pivot := arr[high]
+	i := low-1
+
+
+	for j := low; j < high; j++{
+		if arr[j] < pivot{
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return i+1
 }
 
 
+func main() {
+	arr := []int{6,5,4,3,2,1}
+	quicksort(arr, 0, len(arr)-1)
+
+	fmt.Println( "sorted array:",arr)
+
+}
