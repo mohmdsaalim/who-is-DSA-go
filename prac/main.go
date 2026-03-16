eb83befd2675ad8f8c8b23167016da1c2887cb99
mohmdsaalim <muhammedsaalim005@gmail.com>
2026-03-16 13:56:27 +0530

    undestanded the merge sort logic implmnt in code

--- prac/main.go
+++ prac/main.go
@@ -3,35 +3,33 @@ package main
 import "fmt"
 
 
-func quicksort(arr []int, low, high int)  {
+
+func quick(arr []int, low, high int)  {
 	if low < high{
 		pi := partition(arr, low, high)
 
-		quicksort(arr, low, pi-1)
-		quicksort(arr, pi+1, high)
+		quick(arr, low, pi-1)
+		quick(arr, pi+1, high)
 	}
 }
-
 func partition(arr []int, low, high int) int {
 	pivot := arr[high]
 	i := low-1
 
-
 	for j := low; j < high; j++{
 		if arr[j] < pivot{
 			i++
-			arr[i], arr[j] = arr[j], arr[i]
+			arr[i],arr[j] = arr[j], arr[i]
 		}
 	}
 	arr[i+1], arr[high] = arr[high], arr[i+1]
 	return i+1
 }
 
-
 func main() {
 	arr := []int{6,5,4,3,2,1}
-	quicksort(arr, 0, len(arr)-1)
 
-	fmt.Println( "sorted array:",arr)
+	quick(arr, 0, len(arr)-1)
 
+fmt.Println(arr)
 }
