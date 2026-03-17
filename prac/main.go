0940ed14d6f56cf3436247f343c2393a89213df3
mohmdsaalim <muhammedsaalim005@gmail.com>
2026-03-18 00:37:13 +0530

    merge pending -> merge_sort func?

--- prac/main.go
+++ prac/main.go
@@ -1,44 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
-func mergeSort(arr []int ) []int {
-	if len(arr) <= 1{
-		return arr
-	}
-	mid := len(arr) / 2
-
-	left := mergeSort(arr[:mid])
-	right := mergeSort(arr[mid:])
-
-	return merge(left, right)
-}
-
-func merge(left, right []int) []int {
-	result := []int{}
-
-	i := 0
-	j := 0
-
-	for i < len(left) && j < len(right){
-		if left[i] < right[j]{
-			result = append(result, left[i])
-			i++
-		}else{
-			result = append(result, right[j])
-			j++
-		}
-	}
-	result = append(result, left[i:]...)
-	result = append(result, right[j:]...)
-
-	return result
-}
-
 func main() {
-	arr := []int{6,5,4,3,2,1}
-	a := mergeSort(arr)
-	fmt.Println(a)
+	
+	
 }
